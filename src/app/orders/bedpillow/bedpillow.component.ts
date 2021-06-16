e7fa8ee048c5c271f5d9208183a78443ee1ba280
Profsoul <profsoul23@gmail.com>
2021-06-16 22:40:26 +0530

    Updated in BedPillow Component

--- src/app/orders/bedpillow/bedpillow.component.ts
+++ src/app/orders/bedpillow/bedpillow.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,HostListener } from '@angular/core';
 
 
+
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -20,29 +21,53 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class BedpillowComponent implements OnInit {
   @HostListener('window:resize')
   my_func(){
-   this.windows = window.innerWidth
+   
     if (window.innerWidth<800){
-    this.mobile_view = true;
+    this.windows = window.innerWidth
     
-    
-  }}
-  windows = 1234
+  }
+  else{
+    this.mobile_view=false;
+  }
+}
+
+
+  
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
-  items = [{'name':'suhail'}]
+  windows:any;
+  id:any;
+  items = [{'name':'suhail','no':1,'type':''}]
+  items2 = [{'name':'suhail'}]
+  type:any;
+  count:number = 1;
   mobile_view = true;
 
   constructor() { 
     this.windows = window.innerWidth
+    
   }
   add(){
-    this.items.push({'name':'soul'})
+   this.count = this.items[this.items.length-1]['no']+1 
+   console.log(this.count)
+    this.items.push({'name':'soul','no':this.count,'type':''})
+    
   }
   remove(){
     if(this.items.length != 1){
     this.items.pop()}
 
   }
+  add2(){
+    this.items2.push({'name':'soul'})
+    
+  }
+  remove2(){
+    if(this.items2.length != 1){
+    this.items2.pop()}
+
+  }
+
   ngOnInit(): void {
   }
 
