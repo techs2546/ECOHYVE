5988a74a601fddad19ab9e37aec51afaa2cf9278
Profsoul <profsoul23@gmail.com>
2021-06-20 19:44:23 +0530

    Update Sofa & Cushion

--- src/app/orders/sofacushion/sofacushion.component.ts
+++ src/app/orders/sofacushion/sofacushion.component.ts
@@ -6,8 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sofacushion.component.css']
 })
 export class SofacushionComponent implements OnInit {
-
+id:string='';
+count:number = 1;
+ fileName = '';
+items = [{'no':1,'type':''}]
   constructor() { }
+
+  onFileSelected(event:any) {
+    const file:File = event.target.files[0];
+    if (file) {
+
+        this.fileName = file.name;
+
+        const formData = new FormData();
+
+        formData.append("thumbnail", file);
+        
+    }
+}
+
+add(){
+  this.count = this.items[this.items.length-1]['no']+1 
+   console.log(this.count)
+    this.items.push({'no':this.count,'type':''})
+
+}
+remove(){
+  if(this.items.length != 1){
+    this.items.pop()}
+}
 
   ngOnInit(): void {
   }
